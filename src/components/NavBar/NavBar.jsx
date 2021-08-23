01f3237050326c34fa2e5559ba55f5556a4e750e
euge-ghiso <eugeniaghiso@hotmail.com>
2021-08-23 11:57:39 -0300

    desafio 3

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,27 @@
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
+
+function NavBar(){
+    return (
+        
+            <>
+  
+  <Navbar bg="primary" variant="dark">
+    <Container>
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+
+</>
+
+        
+    )
+}
+
+export default NavBar
