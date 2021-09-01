93604e808f62f39818f95b87399b3c3af046a573
euge-ghiso <eugeniaghiso@hotmail.com>
2021-09-01 13:19:16 -0300

    desafio6bis

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar/NavBar'; 
 import ItemListContainer from './components/container/ItemListContainer'; 
-
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+    <div className="App">    </div>
      
    <NavBar />
+   <Switch>
+   <Route exact path="/">
    <ItemListContainer greeting={"¡¡¡Hola !!! "} />
+   </Route>
 
 
-   <h1> Desafio nro 5 </h1>
-    </div>
+
+  
+    </Switch>
+    </Router>
   );
 }
 
--- src/components/container/ItemListContainer.jsx
+++ src/components/container/ItemListContainer.jsx
@@ -1,29 +1,35 @@
  import "../container/ItemListContainer.css"
  import ItemCount from '../ItemCount';
  import ItemList from '../ItemList';
+ import { useEffect } from 'react';
+
 
  const productos = [
-  { id: 1, nombre: "margarita", precio: 37 ,stock: 5},
-  { id: 2, nombre: "cosmos", precio: 27,stock: 5 },
-  { id: 3, nombre: "lupinos", precio: 40, stock: 5},
-  { id: 4, nombre: "monstera", precio: 55,stock: 5 },
-  { id: 5, nombre: "lavanda", precio: 11,stock: 5 },
-  { id: 6, nombre: "paspalum", precio: 19, stock: 5},
-  { id: 7, nombre: "rosa", precio: 2,stock: 5  }
+  { id: 1, nombre: "margarita",description:"planta", precio: 37 ,stock: 5,pictureUrl:""},
+  { id: 2, nombre: "cosmos",description:"planta", precio: 27,stock: 5,pictureUrl:"" },
+  { id: 3, nombre: "lupinos",description:"planta", precio: 40, stock: 5,pictureUrl:""},
+  { id: 4, nombre: "monstera",description: "planta",precio: 55,stock: 5 ,pictureUrl:""},
+  { id: 5, nombre: "lavanda",description:"planta", precio: 11,stock: 5 ,pictureUrl:""},
+  { id: 6, nombre: "paspalum",description:"planta", precio: 19, stock: 5,pictureUrl:""},
+  { id: 7, nombre: "rosa", description:"planta", precio: 2,stock: 5,pictureUrl:""  }
 ];
 
 
 
 const ItemListContainer = ({greeting}) => {
-  let tarea = new Promise((resolve) => {
- 
-    setTimeout(() => {
+  
+  useEffect(() => {
+       let tarea = new Promise((resolve) => {
+        setTimeout(() => {
         resolve(productos);        
-    }, 2000);
+       }, 2000);
     
   });
 
   tarea.then((respuesta)=>(respuesta))
+}, []).
+
+
     return (
       <>
         <h2> Saludo: {greeting} </h2>
